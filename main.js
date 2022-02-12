73e4376f812eb7cac9c63d83029ded21b55e61b5
galbraithmedia1 <59584919+galbraithmedia1@users.noreply.github.com>
2022-02-12 12:07:41 -0700

    music loaded

---
+++ main.js
@@ -0,0 +1,23 @@
+window.onload = () =>{
+    const song_img_el = document.getElementById('song-img')
+    const song_title_el = document.getElementById('song-title')
+    const song_artist_el = document.getElementById('song-artist')
+    const song_next_up_el = document.getElementById('song-next-up')
+
+    const play_btn = document.getElementById('play-btn')
+    const play_btn_icon = document.getElementById('play-icon')
+    const prev_btn = document.getElementById('prev-btn')
+    const next_btn = document.getElementById('prev-next')
+
+    const audio_player = document.getElementById('music-player')
+
+    let current_song_index;
+    let next_song_index;
+
+    let songs = [
+
+        {
+            title
+        }
+    ]
+}
